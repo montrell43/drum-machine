af1d674ada69ef584fb4d275d20e5e6267dbc16f
montrell43 <tmerriweather@persevernow.org>
2025-07-01 09:31:44 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
+const tock = new Audio('sounds/tock.mp3')
 
+const metronomeToggle = document.getElementById('metronome-toggle')
+
+let count = 1
+const countPlay = document.getElementById('count-display')
 // This function is called every 600ms
 function update() {
-
-    // Play the 'tick' sound
+  if(count < 4) {
+     // Play the 'tick' sound
     tick.play();
+  } else {
+    tock.play()
+  }
+
+  countPlay.textContent = count;
+  count ++;
+  if(count > 4) {
+    count = 1
+  }
+   
 }
 
 // This function sets up update() to be called every 600ms
