9ce7980144a143e469ffcdeb86e5a3e5af5eccd0
montrell43 <tmerriweather@persevernow.org>
2025-07-01 10:10:22 -0500

    add commit

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
 const tock = new Audio('sounds/tock.mp3')
+const kick = new Audio('sounds/kick-drum.mp3')
+const snare = new Audio('sounds/snare-drum.mp3')
+const hihat = new Audio('sounds/hi-hat-drum.mp3')
+
+const kickToggle = document.getElementById('kick-toggle')
+const snareToggle = document.getElementById('snare-toggle')
+const hihatToggle = document.getElementById('hihat-toggle')
+
+const kickBeat = document.getElementById('kick-beat')
+const snareBeat = document.getElementById('snare-beat')
+const hihatBeat = document.getElementById('hihat-beat')
 
 const metronomeToggle = document.getElementById('metronome-toggle')
 
@@ -16,11 +27,25 @@ function update() {
   }
 
   countPlay.textContent = count;
+
+  
+  
+  if(kickToggle.checked) {
+    kick.play()
+  }
+
+  if (snareToggle) {
+    snare.play()
+  } 
+  
+  if (hihatToggle.checked) {
+    hihat.play()
+  }
+  
   count ++;
   if(count > 4) {
     count = 1
   }
-   
 }
 
 // This function sets up update() to be called every 600ms
